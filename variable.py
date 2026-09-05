d2b551024a9f5f900e1c8ec6582c2f65cf789033
Jobaied Chowdhury <jobaiedchowdhuryakash031@gmail.com>
2026-09-05 15:36:34 +0600

    Practice Python variables

---
+++ variable.py
@@ -0,0 +1,7 @@
+name = "Akash"
+age = 22
+goal = "AI Research"
+
+print(name)
+print(age)
+print(goal)
---
+++ variablep2.py
@@ -0,0 +1,9 @@
+research_topic = "AI in Healthcare"
+model_name = "ResNet50"
+training_epochs = 30
+accuracy = 0.95
+
+print(research_topic)
+print(model_name)
+print(training_epochs)
+print(accuracy)
