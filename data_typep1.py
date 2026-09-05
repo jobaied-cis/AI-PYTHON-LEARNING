5f99db9ce01b99a59820b253a4f102e58d003088
Jobaied Chowdhury <jobaiedchowdhuryakash031@gmail.com>
2026-09-05 15:25:10 +0600

    Practice Python data types

---
+++ data_typep1.py
@@ -0,0 +1,9 @@
+model_name = "CNN"
+epochs = 50
+accuracy = 0.9347
+learning_rate = 0.001
+
+print(type(model_name))
+print(type(epochs))
+print(type(accuracy))
+print(type(learning_rate))
---
+++ data_types.py
@@ -0,0 +1,7 @@
+name = "Akash"
+age = 22
+accuracy = 0.9347
+
+print(type(name))
+print(type(age))
+print(type(accuracy))
