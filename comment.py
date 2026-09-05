89366752a554c7a5064b4750a6b7f4e59ab2f8f5
Jobaied Chowdhury <jobaiedchowdhuryakash031@gmail.com>
2026-09-05 15:13:17 +0600

    Complete print practice

---
+++ comment.py
@@ -0,0 +1,5 @@
+"""My Name is Akash
+I am learning Python
+I want to become an AI Researcher"""
+print("""Hello,Everyone
+      As a beginner in Python, I am excited to learn and explore the world of programming. My goal is to become an AI Researcher and contribute to the field of Artificial Intelligence. I am passionate about using technology to solve real-world problems and make a positive impact on society.""")
